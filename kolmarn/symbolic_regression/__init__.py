62c9e102725254f6c710051129941d1565d0dea2
Andrew Garcia <garcia.gtr@gmail.com>
2025-11-08 22:49:40 -0500

    sym_reg branch moved out of kolmarn project until sr algorithm is completely refined

--- kolmarn/symbolic_regression/__init__.py
+++
@@ -1,5 +0,0 @@
-# Post-training TRUE symbolic regression for KolmArn (PySR-based).
-from .sym_layer import discover_symbolic_form, discover_symbolic_layer, export_symbolic
-from .sym_global import  discover_symbolic_global
-
-__all__ = ["discover_symbolic_form", "discover_symbolic_layer", "discover_symbolic_global", "export_symbolic"]
--- kolmarn/symbolic_regression/components/__init__.py
+++
@@ -1,4 +0,0 @@
-from .types import SymbolicResult
-from .config import SymbolicConfig, GlobalSymbolicConfig
-from .utils import _fit_pysr, _get_kan_layer, _sample_unit_component
-from .variables import _HAS_PYSR, _HAS_SYMPY
